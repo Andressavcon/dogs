329c2eb244aa243b4248ff766165bedd3090b029
Andressa Vargas Conrado <andressavconrado@gmail.com>
2023-01-02 19:16:33 -0300

    first commit

---
+++ src/Components/Photo/PhotoComments.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { UserContext } from '../../UserContext';
+import PhotoCommentsForm from './PhotoCommentsForm';
+import styles from './PhotoComments.module.css'
+
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const commentSection = React.useRef(null);
+  const { login } = React.useContext(UserContext);
+
+  React.useEffect(() => {
+    commentSection.current.scrollTop = commentSection.current.scrollHeight;
+  },[comments]);
+
+  return (
+    <>
+      <ul ref={commentSection} className={`${styles.comments} ${props.single ? styles.single : ''}`}>
+        {comments.map(comment => <li key={comment.comment_ID}>
+          <b>{comment.comment_author}: </b>
+          <span>{comment.comment_content}</span>
+        </li>)}
+      </ul>
+
+      {login && <PhotoCommentsForm single={props.single} id={props.id} setComments={setComments} />}
+    </>
+  );
+};
+
+export default PhotoComments;
